86f413a4a4dfcad1c6e698bdaacdd1319986f1c7
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2023-05-26 09:20:15 -0300

    finalizando aplicacao

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 <p align="center">
   <a href="#technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#project">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#files">Files</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#how-to-use">How to Use</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#license">License</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#readme-in-portuguese">README in Portuguese</a>
@@ -24,6 +25,9 @@
 
 ## Project
 
+## Files
+
+
 ## How to Use
 
 ## License
@@ -37,6 +41,7 @@
 <p align="center">
   <a href="#tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#arquivos">Arquivos</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#como-usar">Como Usar</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#licença">Licença</a>
 </p>
@@ -59,6 +64,21 @@
 
 ## Projeto
 
+Este é um projeto que permite adicionar usuários favoritos do Github e exibir algumas informações sobre eles, como repositórios e seguidores. A interface do projeto é uma página HTML que interage com a API do Github para obter os dados dos usuários.
+## Arquivos
+
+- `index.html`: Arquivo HTML que define a estrutura da página e contém a marcação e o layout.
+
+- `style.css`: Arquivo CSS que fornece o estilo visual para a página.
+
+- `main.js`: Arquivo JavaScript que lida com a interatividade da página, incluindo a adição e remoção de usuários favoritos.
+
+- `Favorites.js`: Arquivo JavaScript que contém as classes Favorites e FavoritesView responsáveis por gerenciar os favoritos e exibir a tabela de usuários favoritos.
+
+- `GithubUser.js`: Arquivo JavaScript que contém a classe GithubUser responsável por realizar a pesquisa de usuários no Github usando sua API.
+
 ## Como Usar
+
+- Para acessar ao projeto finalizado, [clique aqui]().
 
 ## Licença
--- js/Favorites.js
+++ js/Favorites.js
@@ -1,30 +1,40 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint).then(data => data.json()).then(({ login, name, public_repos, followers }) => ({
-      login,
-      name,
-      public_repos,
-      followers
-    }))
-  }
-}
+import { GithubUser } from './GithubUser.js'
 
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
-
-    GithubUser.search('maykbrito').then(user => console.log(user))
   }
 
   load() {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
   }
 
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
   async add(username) {
-    const user = await GithubUser.search(username)
+    try {
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if(userExists) {
+        throw new Error('Usuário já cadastrado!')
+      }
+
+      const user = await GithubUser.search(username)
+
+      if(user.login === undefined) {
+        throw new Error('Usuário não encontrado!')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch(error) {
+      alert(error.message)
+    }
   }
 
   delete(user){
@@ -32,6 +42,7 @@ export class Favorites {
 
     this.entries = filteredEntries
     this.update()
+    this.save()
   }
 }
 
@@ -48,7 +59,7 @@ export class FavoritesView extends Favorites {
   onAdd() {
     const addButton = this.root.querySelector('.search button')
     addButton.onclick = () => {
-      const { value } = this.this.root.querySelector('.search input')
+      const { value } = this.root.querySelector('.search input')
 
       this.add(value)
     }
@@ -70,9 +81,10 @@ export class FavoritesView extends Favorites {
 
       row.querySelector('.remove').onclick = () => {
         const isOk = confirm('Tem certeza que deseja deletar essa linha?')
-      }
-      if(isOk) {
-        this.delete(user)
+        
+        if(isOk) {
+          this.delete(user)
+        }
       }
       
       this.tbody.append(row)
